f6fe47c59fdf99c28eba1c80f987bc3895462c99
Isaac Sloan <isaac@isaacsloan.com>
2017-06-10 00:48:20 -0600

    Working amber routes that parses the routes file. Very fast because nothing needs to compile!

--- src/amber_cmd/commands/routes.cr
+++ src/amber_cmd/commands/routes.cr
@@ -4,17 +4,18 @@ require "sentry/sentry_command"
 
 module Amber::CMD
   class MainCommand < Cli::Supercommand
-
     class Routes < Sentry::SentryCommand
+      LABELS = ["Verb", "Controller", "Action", "Pipeline", "Scope", "Resource"]
       command_name "routes"
-      getter routes = JSON::Any.new("")
+      getter routes = Array(Hash(String, String)).new
+      property last_pipe : String?
 
       class Help
         caption "# Print out all defined routes in match order, with names"
       end
 
       def run
-        @routes = get_routes
+        parse_routes
         print_routes_table
       rescue
         puts "Error: Not valid project root directory.".colorize(:red)
@@ -23,28 +24,89 @@ module Amber::CMD
         exit 1
       end
 
-      private def get_routes
-        code = <<-CODE
-          require "amber"
-          require "./src/controller/*"
-          require "./config/*"
-          puts Amber::Server.routes.to_json
-        CODE
-        routes_json = `crystal eval #{code.gsub("\n", ";").inspect}`
-        JSON.parse(routes_json)
+      private def parse_routes
+        File.read_lines("config/routes.cr").each do |line|
+          case line.strip
+          when .starts_with?("routes")
+            set_pipe(line)
+          when .starts_with?("get")
+            set_route(line)
+          when .starts_with?("post")
+            set_route(line)
+          when .starts_with?("patch")
+            set_route(line)
+          when .starts_with?("put")
+            set_route(line)
+          when .starts_with?("delete")
+            set_route(line)
+          when .starts_with?("resources")
+            set_resources(line)
+          end
+        end
+      end
+
+      private def set_route(l)
+        if md = l.to_s.match(/(\w+)\s+\"([^\"]+)\",\s*(\w+),\s*:(\w+)/)
+          route = {"Verb" => md[1]?.to_s}
+          route["Controller"] = md[3]?.to_s
+          route["Action"] = md[4]?.to_s
+          route["Pipeline"] = last_pipe.to_s
+          route["Scope"] = ""
+          route["Resource"] = md[2]?.to_s
+          routes << route
+        end
+      end
+
+      private def set_resources(l)
+        if md = l.to_s.match(/(\w+)\s+\"([^\"]+)\",\s*(\w+)(?:,\s*(\w+)\:\s*\[([^\]]+)\])?/)
+          base_route = md[2]?.to_s
+          controller = md[3]?.to_s
+          filter = md[4]?
+          filter_actions = md[5]?.to_s.gsub(/\:|\s/, "").split(",")
+          {
+            "get" => ["index", "show", "new", "edit"],
+            "post" => ["create"], "patch" => ["update"],
+            "put" => ["update"], "delete" => ["destroy"],
+          }.each do |verb, v|
+            v.each do |action|
+              case filter
+              when "only"
+                next unless filter_actions.includes?(action)
+              when "except"
+                next if filter_actions.includes?(action)
+              end
+              route = {"Verb" => verb}
+              route["Controller"] = controller
+              route["Action"] = action
+              route["Pipeline"] = last_pipe.to_s
+              route["Scope"] = ""
+              route["Resource"] = build_resource(base_route, action)
+              routes << route
+            end
+          end
+        end
+      end
+
+      private def build_resource(route, action)
+        route_end = {"show" => ":id", "new" => "new", "edit" => ":id/edit", "update" => ":id", "destroy" => ":id"}
+        [route, route_end[action]?].compact.join("/")
+      end
+
+      private def set_pipe(l)
+        if md = l.to_s.match(/\:(\w+)/)
+          @last_pipe = md[1]?
+        end
       end
 
       private def print_routes_table
         table = ShellTable.new
-        table.labels = [
-          "Verb", "Controller", "Action", "Pipeline", "Scope", "Resource"
-        ]
+        table.labels = LABELS
         table.label_color = :light_red
         table.border_color = :dark_gray
-        routes.each do |k, v|
+        routes.each do |route|
           row = table.add_row
-          JSON.parse(v.to_s).each do |col, val|
-            row.add_column val.to_s
+          LABELS.each do |l|
+            row.add_column route[l].to_s
           end
         end
         puts table
